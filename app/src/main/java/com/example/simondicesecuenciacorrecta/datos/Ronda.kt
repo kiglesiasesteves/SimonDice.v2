9c69af0f57ce3b2c827e06d1a323f768db2d6b1a
keyla <kiglesiasesteves@danielcastelao.org>
2024-11-18 12:40:16 +0100

    Commit de guardado, la secuencia es la misma, no se iluminan los botones y aún no hemos guardado los estados

---
+++ app/src/main/java/com/example/simondicesecuenciacorrecta/datos/Ronda.kt
@@ -0,0 +1,8 @@
+package com.example.simondicesecuenciacorrecta.datos
+
+import androidx.compose.runtime.mutableStateOf
+
+class Ronda {
+    var ronda = mutableStateOf(0)
+
+}
---
+++ app/src/main/java/com/example/simondicesecuenciacorrecta/datos/SimonColor.kt
@@ -0,0 +1,8 @@
+package com.example.simondicesecuenciacorrecta.datos
+
+enum class SimonColor(val value: Int) {
+    Pink(1),
+    Blue(2),
+    Yellow(3),
+    Green(4)
+}
