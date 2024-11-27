7d0f2e998ff4734ea070a481e53e8830698758d6
keyla <kiglesiasesteves@danielcastelao.org>
2024-11-27 12:57:25 +0100

    Añadir Observer Toast para poder retirar el vaolor del toast

---
+++ app/src/main/java/com/example/simondicesecuenciacorrecta/datos/EstadosToast.kt
@@ -0,0 +1,8 @@
+package com.example.simondicesecuenciacorrecta.datos
+
+enum class EstadosToast(val valor: String ){
+    GANASTE(valor="GANASTE. Enhorabuena!!!"),
+    PERDISTE(valor="PERDISTE. Vuelve a intentarlo!!!"),
+    AUXILIAR("")
+
+}
